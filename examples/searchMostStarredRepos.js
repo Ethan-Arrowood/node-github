fc1707b50983ce215f00730cbb532d0156ec7318
Gregor <gregor@martynus.net>
2017-10-31 14:15:21 -0700

    examples(searchMostStarredRepos): remove callback
    @Ethan-Arrowood we can safely remove the callback here. The error case is handled by `process.on("unhandledRejection", (error) => {...`

--- examples/searchMostStarredRepos.js
+++ examples/searchMostStarredRepos.js
@@ -8,12 +8,4 @@ github.search.repos({
   q: 'stars:>=20000',
   sort: 'stars',
   order: 'desc'
-}, function (err, res) {
-  if (err) throw err
-  for (var itemKey in res['items']) {
-    var item = res['items'][itemKey]
-    var url = item['html_url']
-    var starCount = item['stargazers_count']
-    console.log(url + ' (' + starCount + ')')
-  }
 })
